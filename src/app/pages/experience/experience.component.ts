9ea59dfb8fc23d5dd9a83310e9a6caa67a31226a
chedi-debiche <ch.debiche@gmail.com>
2025-11-13 21:53:17 +0100

    commit2

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ExperienceService } from '../../core/experience-service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor , NgIf],
   templateUrl: './experience.html',
   styleUrl: './experience.css',
 })
